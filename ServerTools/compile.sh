72628e834c993a05039c82da791e11825d052019
Bob Evans <rbe5000@gmail.com>
2012-07-26 16:52:47 -0700

    made paths relative

--- ServerTools/compile.sh
+++ ServerTools/compile.sh
@@ -1,1 +1,1 @@
-javac -d bin -cp lib:/Users/bobevans/applications/appengine-java-sdk-1.7.0/lib/impl/appengine-api.jar:/Users/bobevans/applications/appengine-java-sdk-1.7.0/lib/appengine-remote-api.jar:/Users/bobevans/git/paco/Paco-Server/war/WEB-INF/lib/guava-r09.jar:bin src/com/google/paco/cmdline/RemoteClient.java
+javac -d bin -cp lib:../tools/appengine-java-sdk-1.5.0.1/lib/impl/appengine-api.jar:../tools/appengine-java-sdk-1.5.0.1/lib/appengine-remote-api.jar:../Paco-Server/war/WEB-INF/lib/guava-r09.jar:bin src/com/google/paco/cmdline/RemoteClient.java
--- ServerTools/run.sh
+++ ServerTools/run.sh
@@ -1,1 +1,1 @@
-java -cp lib:/Users/bobevans/applications/appengine-java-sdk-1.7.0/lib/impl/appengine-api.jar:/Users/bobevans/applications/appengine-java-sdk-1.7.0/lib/appengine-remote-api.jar:/Users/bobevans/git/paco/Paco-Server/war/WEB-INF/lib/guava-r09.jar:bin com.google.paco.cmdline.RemoteClient
+java -cp lib:../tools/appengine-java-sdk-1.5.0.1/lib/impl/appengine-api.jar:../tools/appengine-java-sdk-1.5.0.1/lib/appengine-remote-api.jar:../Paco-Server/war/WEB-INF/lib/guava-r09.jar:bin com.google.paco.cmdline.RemoteClient
