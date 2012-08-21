1db63a26c4fa9e97abc22d83f00370db974eb98f
Bob Evans <rbe5000@gmail.com>
2012-08-21 16:40:03 -0700

    clean up testing and create ant task for running android emulator tests

--- PacoTest/tests/com/google/android/apps/paco/ESMSignalGeneratorTest.java
+++ PacoTest/tests/com/google/android/apps/paco/ESMSignalGeneratorTest.java
@@ -14,7 +14,7 @@
 * specific language governing permissions and limitations
 * under the License.
 */
-package com.google.android.apps.paco.test;
+package com.google.android.apps.paco;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- PacoTest/tests/com/google/android/apps/paco/TimeUtilTest.java
+++ PacoTest/tests/com/google/android/apps/paco/TimeUtilTest.java
@@ -1,4 +1,4 @@
-package com.google.android.apps.paco.test;
+package com.google.android.apps.paco;
 
 import org.joda.time.DateTime;
 
