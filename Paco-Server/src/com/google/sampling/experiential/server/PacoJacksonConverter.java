acab7cbd7375b655821d28528ed0cfe7a78821c7
Cory Cornelius <cory.cornelius@google.com>
2012-09-04 11:29:23 -0700

    Don't fail on unknown properties.

--- Paco-Server/src/com/google/sampling/experiential/server/PacoJacksonConverter.java
+++ Paco-Server/src/com/google/sampling/experiential/server/PacoJacksonConverter.java
@@ -4,6 +4,7 @@ package com.google.sampling.experiential.server;
 
 import com.google.paco.shared.model.Experiment;
 
+import org.codehaus.jackson.map.DeserializationConfig.Feature;
 import org.restlet.engine.Engine;
 import org.restlet.engine.converter.ConverterHelper;
 import org.restlet.ext.jackson.JacksonConverter;
@@ -11,6 +12,7 @@ import org.restlet.representation.Representation;
 import org.restlet.representation.Variant;
 import org.restlet.resource.Resource;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -50,6 +52,21 @@ public class PacoJacksonConverter extends JacksonConverter {
     }
 
     return super.toRepresentation(object, variant, resource);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * org.restlet.ext.jackson.JacksonConverter#toObject(org.restlet.representation.Representation,
+   * java.lang.Class, org.restlet.resource.Resource)
+   */
+  @Override
+  public <T> T toObject(Representation source, Class<T> target, Resource resource)
+      throws IOException {
+    getObjectMapper().configure(Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    return super.toObject(source, target, resource);
   }
 
   public static void replaceConverter() {
