9696cec8d5b8e3aad301d14b45f05398d82d67e4
Bob Evans <rbe5000@gmail.com>
2012-10-03 15:35:04 -0700

    tests for whitelist class to check email

---
+++ Paco-Server/tests/com/google/sampling/experiential/shared/WhitelistTest.java
@@ -0,0 +1,44 @@
+package com.google.sampling.experiential.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WhitelistTest {
+
+  @Test
+  public void testBlank() {
+    assertFalse(new Whitelist().allowed(null));
+  }
+  
+  @Test
+  public void testEmpty() {
+    assertFalse(new Whitelist().allowed(""));
+  }
+
+  @Test
+  public void testNameNoDomain() {
+    assertFalse(new Whitelist().allowed("bob@"));
+  }
+  
+  @Test
+  public void testGooglerDomain() {
+    assertFalse(new Whitelist().allowed("google.com"));
+  }
+
+  @Test
+  public void testGoogler() {
+    assert(new Whitelist().allowed("bob@google.com"));
+  }
+  
+  @Test
+  public void testGooglerFakeDouble() {
+    assertFalse(new Whitelist().allowed("bob@google.com@google.com"));
+  }
+
+  @Test
+  public void testGooglerFakeBlank() {    
+    assertFalse(new Whitelist().allowed("@google.com"));
+  }
+
+}
