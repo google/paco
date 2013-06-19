76ab78efc4dff3c32b3321f3130824c59cbafde7
Priya Donti <donti@google.com>
2013-06-18 17:54:56 -0700

    issue146: fixed server Experiment such that it only has Dates (not strings) as data members.  working on unit test for DatabaseHelper migration, which is currently too large.

--- PacoTest/tests/com/google/android/apps/paco/DatabaseHelperTest.java
+++ PacoTest/tests/com/google/android/apps/paco/DatabaseHelperTest.java
@@ -1,11 +1,14 @@
 package com.google.android.apps.paco;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.HashMap;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
@@ -24,15 +27,22 @@ public class DatabaseHelperTest extends AndroidTestCase {
     DatabaseHelper oldDbHelper = new DatabaseHelper(getContext(), v13DB, 13);
     SQLiteDatabase oldDb = oldDbHelper.getReadableDatabase();
     
-    DatabaseHelper dbHelper = new DatabaseHelper(getContext(), v13DB_TEST, 14);
-    SQLiteDatabase db = dbHelper.getReadableDatabase();
+    DatabaseHelper newDbHelper = new DatabaseHelper(getContext(), v13DB_TEST, 14);
+    SQLiteDatabase newDb = newDbHelper.getReadableDatabase();
     
-    // old cursor
-    // new cursor
-    // comparison
+    String[] columns = {ExperimentColumns._ID, ExperimentColumns.FIXED_DURATION, 
+                        ExperimentColumns.START_DATE, ExperimentColumns.END_DATE};
+    Cursor oldCursor = oldDb.query(ExperimentProvider.EXPERIMENTS_TABLE_NAME, columns, null, null, null, null, null);
+    Cursor newCursor = newDb.query(ExperimentProvider.EXPERIMENTS_TABLE_NAME, columns, null, null, null, null, null);
     
+    assertNotNull(oldCursor);
+    assertNotNull(newCursor);
     
+    checkDateEquality(oldCursor, newCursor);
+  
+    closeFiles(oldCursor, newCursor);
   }
+
   
   private void copy13db() throws IOException {
     InputStream in = new FileInputStream(v13DB);
@@ -44,6 +54,46 @@ public class DatabaseHelperTest extends AndroidTestCase {
     }
     in.close();
     out.close();
+  }
+  
+  private void checkDateEquality(Cursor oldCursor, Cursor newCursor) {
+    while (oldCursor.moveToNext()) {
+      newCursor.moveToNext();
+      if (isFixedDuration(oldCursor)) {
+        checkStartDate(oldCursor, newCursor);
+        checkEndDate(oldCursor, newCursor);
+      } 
+    }
+  }
+  
+  private boolean isFixedDuration(Cursor cursor) {
+    return ( cursor.getLong(cursor.getColumnIndex(ExperimentColumns.FIXED_DURATION)) == 1 );
+  }
+  
+  private void checkStartDate(Cursor oldCursor, Cursor newCursor) {
+    checkDate(oldCursor, newCursor, ExperimentColumns.START_DATE);
+  }
+  
+  private void checkEndDate(Cursor oldCursor, Cursor newCursor) {
+    checkDate(oldCursor, newCursor, ExperimentColumns.END_DATE);
+  }
+  
+  private void checkDate(Cursor oldCursor, Cursor newCursor, String columnName) {
+    String oldDate = formatDateAsStr(oldCursor.getLong(oldCursor.getColumnIndex(columnName)));
+    String newDate = newCursor.getString(newCursor.getColumnIndex(columnName));
+    assertEquals(oldDate, newDate);
+  }
+  
+  private String formatDateAsStr(Long dateLong) {
+    return TimeUtil.formatDate(dateLong);
+  }
+  
+  private void closeFiles(Cursor oldCursor, Cursor newCursor) {
+    newCursor.close();
+    oldCursor.close();
+    
+    File newDbFile = new File(v13DB_TEST);
+    newDbFile.delete();
   }
 
 }
