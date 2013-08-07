29fee6d6c4a1bb204b75a5b5cf03c3baaa06c15c
Bob Evans <rbe5000@gmail.com>
2013-08-07 01:10:27 -0700

    big refactor util files

---
+++ Paco-Server/src/com/google/sampling/experiential/shared/TimeUtil.java
@@ -0,0 +1,14 @@
+package com.google.sampling.experiential.shared;
+
+public class TimeUtil {
+
+  public static final String DATETIME_FORMAT = "yyyy/MM/dd HH:mm:ssZ";
+  public static final String DATETIME_FORMAT_FOR_FILENAME = "yyyy_MM_dd_HH_mm_ssZ";
+  public static final String DATETIME_FORMAT_OLD = "yyyyMMdd:HH:mm:ssZ";
+  public static final String DATE_FORMAT = "yyyy/MM/dd";
+  public static final String DATE_WITH_ZONE_FORMAT = "yyyy/MM/ddZ";
+
+  public static final int MILLIS_IN_A_DAY = 24 * 60 * 60 * 1000;
+  public static final int EXTRA_MILLIS_OFFSET = 50000;
+
+}
