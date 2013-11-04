a90d4cf0d5096b303936b7e9331dab7fd2e35b04
Yiming Zhang <yimingcheung@hotmail.com>
2013-11-04 11:37:28 -0800

    Issue #209 : add place-holder unit tests for weekly and monthly ESM schedule generator

---
+++ Paco-iOS/PacoTests/PacoScheduleGeneratorMonthlyESMTests.m
@@ -0,0 +1,59 @@
+/* Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#import <SenTestingKit/SenTestingKit.h>
+#import "PacoScheduleGenerator+ESM.h"
+#import "PacoExperimentSchedule.h"
+#import "PacoExperiment.h"
+#import "PacoExperimentDefinition.h"
+#import "PacoDateUtility.h"
+#import "NSDate+Paco.h"
+
+@interface PacoScheduleGeneratorMonthlyESMTests : SenTestCase
+
+@end
+
+@implementation PacoScheduleGeneratorMonthlyESMTests
+
+- (void)setUp {
+  [super setUp];
+  // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown {
+  // Put teardown code here; it will be run once, after the last test case.
+  [super tearDown];
+}
+
+- (void)testDatesToScheduleForESMExperiment {
+  STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testEsmCycleStartDateForSchedule {
+  STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testNextCycleStartDateForSchedule {
+  STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testGenerateESMDatesForExperiment {
+  STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+- (void)testCreateESMScheduleDates {
+  STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
