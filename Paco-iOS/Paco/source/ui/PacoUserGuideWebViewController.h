36f80f0bcb8d005e9ed432185ed2ea862391938c
Dhanya <dhanya.chengappa@above-inc.com>
2013-11-15 00:33:52 +0530

    adding help.html file

---
+++ Paco-iOS/Paco/source/ui/PacoUserGuideWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  PacoUserGuideWebViewController.h
+//  Paco
+//
+//  Created by Dhanya Chengappa on 15/11/13.
+//  Copyright (c) 2013 Paco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PacoUserGuideWebViewController : UIViewController
+
+@end
---
+++ Paco-iOS/Paco/source/ui/PacoUserGuideWebViewController.m
@@ -0,0 +1,38 @@
+//
+//  PacoUserGuideWebViewController.m
+//  Paco
+//
+//  Created by Dhanya Chengappa on 15/11/13.
+//  Copyright (c) 2013 Paco. All rights reserved.
+//
+
+#import "PacoUserGuideWebViewController.h"
+
+@interface PacoUserGuideWebViewController ()
+
+@end
+
+@implementation PacoUserGuideWebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
