7137abf78e6f19e4c66ea981ce94d8092f25bdc2
Yiming Zhang <yimingcheung@hotmail.com>
2014-01-17 15:04:18 -0800

    #636 keep running experiments even if server is no longer returning its definition when user refreshes

--- Paco-iOS/Paco/source/core/model/PacoModel.h
+++ Paco-iOS/Paco/source/core/model/PacoModel.h
@@ -66,7 +66,7 @@ extern NSString* const PacoFinishRefreshing;
 
 - (BOOL)hasRunningExperiments;
 
-- (void)refreshExperimentsWithBlock:(void(^)(BOOL shouldRefreshSchedules, NSArray* deletedExperimentIds))block;
+- (void)refreshExperimentsWithBlock:(void(^)(BOOL shouldRefreshSchedules))block;
 
 @end
 
