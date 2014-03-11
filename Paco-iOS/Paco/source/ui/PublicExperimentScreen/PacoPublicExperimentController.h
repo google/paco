6fc63f0ae7a64fe6eef9f0fece47a03825ad00c2
Yiming Zhang <yimingcheung@hotmail.com>
2014-03-11 11:16:43 -0700

    #783 add placeholder view controller for public experiments

---
+++ Paco-iOS/Paco/source/ui/PublicExperimentScreen/PacoPublicExperimentController.h
@@ -0,0 +1,13 @@
+//
+//  PacoPublicExperimentController.h
+//  Paco
+//
+//  Created by Yiming Zhang on 3/11/14.
+//  Copyright (c) 2014 Paco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PacoPublicExperimentController : UIViewController
+
+@end
