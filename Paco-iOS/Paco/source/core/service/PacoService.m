f12e0e3567bc1e89b836359dd64a64109f5afeb5
Yiming Zhang <yimingcheung@hotmail.com>
2014-03-12 21:33:32 -0700

    iOS: fix definition id overflow bug

--- Paco-iOS/Paco/source/core/service/PacoService.m
+++ Paco-iOS/Paco/source/core/service/PacoService.m
@@ -173,7 +173,7 @@
       for (NSDictionary* dict in definitionList) {
         NSNumber* idNum = [dict objectForKey:@"id"];
         NSAssert(idNum != nil && [idNum isKindOfClass:[NSNumber class]], @"idNum should be valid!");
-        NSString* definitionId = [NSString stringWithFormat:@"%ld", [idNum longValue]];
+        NSString* definitionId = [NSString stringWithFormat:@"%lld", [idNum longLongValue]];
         [result addObject:definitionId];
       }
       if (completionBlock) {
