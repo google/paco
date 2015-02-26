e185a0eb0e7703e2becfa562da70ccd8c6dced85
Ian Spiro <ispiro@google.com>
2015-02-25 17:22:59 -0800

    Adds more partials

--- Paco-Angular/app/js/controllers.js
+++ Paco-Angular/app/js/controllers.js
@@ -5,11 +5,33 @@ var app = angular.module('pacoControllers', []);
 
 app.controller('ExperimentCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
 
-  $scope.responseTypes = ["likert", "likert_smileys", "open text", "list", "photo", "location"];
   $scope.experimentIdx = parseInt($routeParams.experimentIdx);
   $scope.previousIdx = -1;
   $scope.nextIdx = -1;
   $scope.selectedIndex = 1;
+  $scope.loaded = false;
+
+  $http.get('js/experiments.json').success(function(data) {
+    $scope.experiment = data[$scope.experimentIdx];
+    $scope.db = data;
+    
+    if ($scope.experimentIdx < data.length - 1) {
+      $scope.nextIdx = $scope.experimentIdx + 1;
+    }
+
+    if ($scope.experimentIdx > 0) {
+      $scope.previousIdx = $scope.experimentIdx - 1;
+    }
+    
+    $scope.loaded = true;
+    $scope.$broadcast('experimentChange');
+  });
+}]);
+
+
+app.controller('InputsCtrl', ['$scope', function($scope) {
+
+  $scope.responseTypes = ["likert", "likert_smileys", "open text", "list", "photo", "location"];
 
   $scope.removeInput = function(idx) {
     $scope.experiment.inputs.splice(idx, 1);
@@ -18,32 +40,24 @@ app.controller('ExperimentCtrl', ['$scope', '$http', '$routeParams', function($s
   $scope.removeChoice = function(input, idx) {
     input.splice(idx, 1);
   };
-
-  $http.get('js/experiments.json').success(function(data) {
-    $scope.experiment = data[$scope.experimentIdx];
-
-    if ($scope.experimentIdx < data.length - 1) {
-      $scope.nextIdx = $scope.experimentIdx + 1;
-    }
-
-    if ($scope.experimentIdx > 0) {
-      $scope.previousIdx = $scope.experimentIdx - 1;
-    }
-  });
 }]);
 
-app.controller('GroupCtrl', ['$scope', function($scope) {
-  $scope.expand = true;
+
+app.controller('ExpandCtrl', ['$scope', function($scope) {
+  $scope.expand = $scope.loaded;
 
   $scope.toggleExpand = function() {
     $scope.expand = !$scope.expand;
   }
+
+  $scope.$on('experimentChange', function(event, args) {
+    $scope.expand = true;
+  });
 }]);
 
 
 app.controller('ScheduleCtrl', ['$scope', function($scope) {
 
-  $scope.scheduleTypes = ["Daily", "Weekdays", "Weekly", "Monthly", "Random sampling (ESM)", "Self Report"];
   $scope.scheduleTypes = ["Daily", "Weekdays", "Weekly", "Monthly", "Random sampling (ESM)", "Self Report"];
   $scope.repeatRates = range(1, 30);
   $scope.daysOfMonth = range(1, 31);
@@ -64,20 +78,22 @@ app.controller('ScheduleCtrl', ['$scope', function($scope) {
     times.splice(idx + 1, 0, {'fixedTimeMillisFromMidnight': 0});
   };
 
+  $scope.$watchCollection('experiment.schedule.days', function(days) {
 
-  $scope.$watchCollection('experiment.schedule.days', function() {
-    console.log($scope.experiment.schedule.days);
     var sum = 0;
-    for (var i = 0; i < 7; i++) {
-      if ($scope.experiment.schedule.days[i]) {
-        sum += Math.pow(2, i);
+
+    if (days) {
+      for (var i = 0; i < 7; i++) {
+        if ($scope.experiment.schedule.days[i]) {
+          sum += Math.pow(2, i);
+        }
       }
+      $scope.experiment.schedule.weekDaysScheduled = sum;
     }
-    $scope.experiment.schedule.weekDaysScheduled = sum;
   });
   
-  $scope.$watch('experiment.schedule.scheduleType', function() {
-    if ($scope.experiment.schedule.signalTimes.length == 0) {
+  $scope.$watch('experiment.schedule.scheduleType', function(times) {
+    if (times) {
       $scope.experiment.schedule.signalTimes = [{}]; 
     }
   });
