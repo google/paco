a2ffc6a4232ad09c2b34f6ae85be50f4679eb466
Ian Spiro <ispiro@google.com>
2015-03-18 13:12:59 -0400

    Fixes bug in millis directive when current system time is on DST

--- Paco-Angular/app/js/directives.js
+++ Paco-Angular/app/js/directives.js
@@ -1,17 +1,32 @@
+/**
+ * This directive uses two-way data filtering to convert between the time
+ * returned by an HTML time input (a timestamp relative to midnight, 12/31/1969) 
+ * and the the format that PACO uses, millisSinceMidnight.
+ */
+
 app.directive('milli', function() {
   return {
     restrict: 'A',
     require: 'ngModel',
     link: function(scope, element, attr, ngModel) {
-      var UTCOffset = 60 * 1000 * (new Date()).getTimezoneOffset();
+
+      /* 
+       * Since the time input is relative to 12/31/1969, it will never
+       * include daylight savings time. So to compute the millisecond delta, we
+       * make a date in January, which will be in the current system timezone,
+       * then call getTimezoneOffset on that date.
+       */
+      var now = new Date();
+      var nonDSTDate = new Date(now.getFullYear(), 0, 1);
+      var UTCOffset = 60 * 1000 * nonDSTDate.getTimezoneOffset();
 
       function dateToMillis(text) {
         var dd = Date.parse(text);
         return dd - UTCOffset;
       }
 
-      function millisToDate(text) {
-        return new Date(parseInt(text) + UTCOffset);
+      function millisToDate(millis) {
+        return new Date(parseInt(millis) + UTCOffset);
       }
       ngModel.$parsers.push(dateToMillis);
       ngModel.$formatters.push(millisToDate);
