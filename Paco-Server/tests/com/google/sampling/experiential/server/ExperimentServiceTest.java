0d054c2ad3bd4e1adc462c5139f19163361928de
Bob Evans <rbe5000@gmail.com>
2015-03-19 14:20:33 -0700

    all experiment service tests passing

--- Paco-Server/tests/com/google/sampling/experiential/server/ExperimentServiceTest.java
+++ Paco-Server/tests/com/google/sampling/experiential/server/ExperimentServiceTest.java
@@ -63,7 +63,7 @@ public class ExperimentServiceTest extends TestCase {
   public void testRetrieveMatchingExperimentsTwoExperimentIds() {
     createAndSaveExperiment(ExperimentTestConstants.TEST_EXPERIMENT_0_NEW);
     createAndSaveExperiment(ExperimentTestConstants.TEST_EXPERIMENT_1_NEW);
-    List<Long> experimentList = Lists.newArrayList(1l, 6l);
+    List<Long> experimentList = Lists.newArrayList(1l, 4l);
     List<ExperimentDAO> experiments = ExperimentServiceFactory.getExperimentService().getExperimentsById(experimentList, email, null);
     assertTrue(experiments.size() == 2);
     assertEquals(experiments.get(0).getId(), experimentList.get(0));
@@ -72,20 +72,24 @@ public class ExperimentServiceTest extends TestCase {
 
   public void testRetrieveMyExperiments() throws Exception {
     createAndSaveExperiment(ExperimentTestConstants.TEST_EXPERIMENT_USER1_ADMIN_NEW);
+    helper.setEnvEmail("user2@gmail.com"); // saving as use1 makes user1 an admin and creator.
     createAndSaveExperiment(ExperimentTestConstants.TEST_EXPERIMENT_USER2_ADMIN_NEW);
+    helper.setEnvEmail("user2@gmail.com");
     DateTimeZone email1Timezone = DateTime.now().getZone();
     List<ExperimentDAO> experiments = ExperimentServiceFactory.getExperimentService().getMyJoinableExperiments("user1@gmail.com", email1Timezone, null, null).getExperiments();
     assertTrue(experiments.size() == 1);
-    assertEquals("PhotoMonkeyUser1", experiments.get(0).getTitle());
+    assertEquals("Test 5", experiments.get(0).getTitle());
 
     experiments = ExperimentServiceFactory.getExperimentService().getMyJoinableExperiments("user2@gmail.com", email1Timezone, null, null).getExperiments();
     assertTrue(experiments.size() == 2);
-    assertEquals("PhotoMonkeyUser1", experiments.get(0).getTitle());
-    assertEquals("PhotoMonkeyUser2", experiments.get(1).getTitle());
+    assertEquals("Test 5", experiments.get(0).getTitle());
+    assertEquals("Test 6", experiments.get(1).getTitle());
   }
 
   public void testRetrieveMyExperimentsUserPublishedUserButNotPublished() throws Exception {
+    helper.setEnvEmail("user2@gmail.com");
     createAndSaveExperiment(ExperimentTestConstants.TEST_EXPERIMENT_USER1_PUBLISHED_USER_NOT_PUBLISHED_NEW);
+    helper.setEnvEmail("user1@gmail.com");
     DateTimeZone email1Timezone = DateTime.now().getZone();
     List<ExperimentDAO> experiments = ExperimentServiceFactory.getExperimentService().getMyJoinableExperiments("user1@gmail.com", email1Timezone, null, null).getExperiments();
     assertTrue(experiments.size() == 0);
