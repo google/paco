27c2c381c5219dfeae902e8776cb24290be03aa2
northropo <northropo@northropo-macbookpro.roam.corp.google.com>
2015-07-20 18:27:15 -0700

    added unit tests for j2objc

---
+++ Paco-iOS/Paco/source/PacoTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  PacoTestViewController.h
+//  Paco
+//
+//  Created by northropo on 7/17/15.
+//  Copyright (c) 2015 Paco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PacoTestViewController : UIViewController
+
+@end
