585f33a131b9b89623e905429e2fffb772f51626
Ian Spiro <ispiro@google.com>
2016-04-05 16:19:45 -0700

    Uses group picker to filter individual user stats

--- Paco-Server/ear/default/web/js/directives.js
+++ Paco-Server/ear/default/web/js/directives.js
@@ -380,6 +380,30 @@ pacoApp.filter('percent', ['$filter', function ($filter) {
 }]);
 
 
+pacoApp.filter('timeSince', ['$filter', function ($filter) {
+  return function(millis) {
+    var seconds = Math.floor(((new Date().getTime()/1000) - millis/1000))
+    interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) return interval + 'y';
+
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) return interval + 'm';
+
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) return interval + 'd';
+
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) return interval + 'h';
+
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) return interval + 'm ';
+
+    return Math.floor(seconds) + 's';
+  }
+}]);
+
+
 pacoApp.filter('jsonToTable', ['util', 'config', function(util, config) {
 
   return function (json, unpackResponse){
