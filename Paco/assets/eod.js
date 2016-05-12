7eaa7a58a9aee372266be58d25ac17ac7edf22de
Bob Evans <rbe5000@gmail.com>
2016-05-12 00:33:48 -0700

    fix multiselect list choice name display error

--- Paco/assets/eod.js
+++ Paco/assets/eod.js
@@ -1028,9 +1028,10 @@ paco.renderer = (function() {
       if (!input) {
         continue;
       }
+      
       responsesHtml += "<div class=\"row\" style=\"margin-bottom: 8px;\">";
-      responsesHtml += "<h6 class=\"left indigo-text\">";
-      responsesHtml += input.text;
+      responsesHtml += "<h6 class=\"left indigo-text\">";      
+      responsesHtml += input.text;      
       responsesHtml += "</h6><br>";
       responsesHtml += "<p class=\"black-text\">";
       responsesHtml += "&nbsp;&nbsp;&nbsp;"
@@ -1048,14 +1049,14 @@ paco.renderer = (function() {
           if (!input.multiselect) {
             answer = parseInt(answer);
             var index = answer;
-            listChoiceName = input.listChoices[index];
+            listChoiceName = input.listChoices[index - 1];
           } else {
             var indices = answer.split(",");
-            for (var i = 0; i < indices.length; i++) {
-              if (i > 0) {
+            for (var j = 0; j < indices.length; j++) {
+              if (j > 0) {
                 listChoiceName += ", ";
               }
-              var index = indices[i]; 
+              var index = indices[j]; 
               index -= 1;
               if (index < 0) {
                 index = 0;
@@ -1279,6 +1280,7 @@ paco.executeEod = (function() {
     var renderEvent = function() {
       unsavedEdits = false;
       var currentEvent = unfinishedDailyEvents[currentPingIndex];
+      
       form_root.append(paco.renderer.renderDailyPingResponsesPanel(referredExperimentGroup.inputs, 
           currentEvent, currentPingIndex, pingCount));
       if (submitted.indexOf(currentEvent.responseTime) != -1) {
