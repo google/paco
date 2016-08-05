97ad98a24c7be9a3342b5cb32299749659bb8984
Bram Bonné <brambonne@google.com>
2016-08-05 15:05:25 -0700

    Added documentation to isLocalDevelopmentServerAddress,
    noting how this could be useful.

--- Paco/src/com/pacoapp/paco/net/ServerAddressBuilder.java
+++ Paco/src/com/pacoapp/paco/net/ServerAddressBuilder.java
@@ -16,6 +16,17 @@ public class ServerAddressBuilder {
     }
   }
 
+  /**
+   * Check whether Paco is connecting to a local development webserver.
+   * We include localhost (127.0.0.1) here because a very easy way to get your phone to connect to a
+   * testserver running on the machine it's connected to, is by just forwarding its local port to a
+   * port on the machine, like this:
+   * adb reverse tcp:8080 tcp:8080
+   * Then the phone, whatever machine its connected to through adb, can just use 127.0.0.1:8080 as
+   * the server address to connect to the host.
+   * @param serverAddress The server address as set in Paco's settings
+   * @return whether this is a local development address
+   */
   public static boolean isLocalDevelopmentServerAddress(String serverAddress) {
     return serverAddress.contains("10.0.2.2") || serverAddress.contains("127.0.0.1");
   }
