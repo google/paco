a88530f674b8e53dfc4b2a626bc37f4449ae5bbd
Bram Bonné <brambonne@google.com>
2016-08-18 18:03:22 -0700

    Added test for non-determinism

--- PacoTest/src/com/google/android/apps/paco/test/CryptoTest.java
+++ PacoTest/src/com/google/android/apps/paco/test/CryptoTest.java
@@ -8,7 +8,8 @@ import com.pacoapp.paco.model.Event;
 import com.pacoapp.paco.model.Output;
 import com.pacoapp.paco.net.Crypto;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -30,10 +31,21 @@ public class CryptoTest extends AndroidTestCase {
   private Crypto crypto;
   private Method base64ToPublicKey;
   private Method encryptAnswer;
+  private KeyPair keyPair;
+  private Output output;
 
   @Before
   public void setUp() throws Exception {
     crypto = new Crypto(getContext());
+
+    KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+    keyGen.initialize(2048);
+    keyPair = keyGen.genKeyPair();
+
+    output = new Output();
+    output.setName("name");
+    output.setAnswer("answer");
+
     // Make necessary methods accessible
     base64ToPublicKey = crypto.getClass().getDeclaredMethod("base64ToPublicKey", String.class);
     base64ToPublicKey.setAccessible(true);
@@ -50,19 +62,11 @@ public class CryptoTest extends AndroidTestCase {
 
     PublicKey decoded = (PublicKey) base64ToPublicKey.invoke(crypto, encoded64);
 
-    Assert.assertArrayEquals(publicKey, decoded.getEncoded());
+    assertArrayEquals(publicKey, decoded.getEncoded());
   }
 
   @Test
   public void testEncryptAnswer() throws NoSuchAlgorithmException, InvocationTargetException, IllegalAccessException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
-    KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-    keyGen.initialize(2048);
-    KeyPair keyPair = keyGen.genKeyPair();
-
-    Output output = new Output();
-    output.setName("name");
-    output.setAnswer("answer");
-
     Output encryptedOutput = null;
     try {
       encryptedOutput = (Output) encryptAnswer.invoke(crypto, output, keyPair.getPublic());
@@ -78,5 +82,26 @@ public class CryptoTest extends AndroidTestCase {
     String decryptedAnswer = new String(decryptedBytes, "UTF-8");
 
     assertEquals(decryptedAnswer, "answer");
+  }
+
+  /**
+   * This test makes sure that encrypting the same string twice would yield a different result. This
+   * is part of non-determinism, which is needed because otherwise someone with access to the server
+   * DB could encrypt a string of plaintext with the public key of the experiment organizer, and
+   * check if the encrypted text corresponds to any data in the database.
+   */
+  @Test
+  public void testNonDeterminism() {
+    try {
+      Output encryptedOutput1 = (Output) encryptAnswer.invoke(crypto, output, keyPair.getPublic());
+      Output output2 = new Output();
+      output2.setName("name");
+      output2.setAnswer("answer");
+      Output encryptedOutput2 = (Output) encryptAnswer.invoke(crypto, output2, keyPair.getPublic());
+
+      assertThat(encryptedOutput1.getAnswer(), not(equalTo(encryptedOutput2.getAnswer())));
+    } catch (Exception e) {
+      fail("Exception when trying to encrypt: " + e);
+    }
   }
 }
