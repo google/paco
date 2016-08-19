dd6e955014954ecbf0101d69063706c55526cc05
Bram Bonné <brambonne@google.com>
2016-08-19 15:14:04 -0700

    Added tests for new crypto functions, including a full test

--- PacoTest/src/com/google/android/apps/paco/test/CryptoTest.java
+++ PacoTest/src/com/google/android/apps/paco/test/CryptoTest.java
@@ -5,8 +5,11 @@ import android.util.Base64;
 import android.util.Log;
 
 import com.pacoapp.paco.model.Event;
+import com.pacoapp.paco.model.Experiment;
+import com.pacoapp.paco.model.ExperimentProviderUtil;
 import com.pacoapp.paco.model.Output;
 import com.pacoapp.paco.net.Crypto;
+import com.pacoapp.paco.shared.model2.ExperimentDAO;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -16,16 +19,27 @@ import org.junit.Test;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class CryptoTest extends AndroidTestCase {
   private Crypto crypto;
@@ -33,23 +47,44 @@ public class CryptoTest extends AndroidTestCase {
   private Method encryptAnswer;
   private KeyPair keyPair;
   private Output output;
+  private List<Output> outputList;
+  private ExperimentProviderUtil experimentProviderUtil;
+  private IvParameterSpec ivParameterSpec;
+  private SecretKey secretKey;
 
   @Before
   public void setUp() throws Exception {
-    crypto = new Crypto(getContext());
+    experimentProviderUtil = new MockExperimentProviderUtil(getContext());
+    crypto = new Crypto(experimentProviderUtil);
 
     KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
     keyGen.initialize(2048);
     keyPair = keyGen.genKeyPair();
+    SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+    byte[] iv = new byte[16];
+    secureRandom.nextBytes(iv);
+    ivParameterSpec = new IvParameterSpec(iv);
+
+    KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+    keyGenerator.init(192);
+    secretKey = keyGenerator.generateKey();
 
     output = new Output();
     output.setName("name");
     output.setAnswer("answer");
+    outputList = Arrays.asList(output);
+
+    ExperimentDAO dao = new ExperimentDAO();
+    dao.setPublicKey(Base64.encodeToString(keyPair.getPublic().getEncoded(), Base64.NO_WRAP));
+    Experiment experiment = new Experiment();
+    experiment.setExperimentDAO(dao);
+    experiment.setServerId(42L);
+    experimentProviderUtil.insertFullJoinedExperiment(experiment);
 
     // Make necessary methods accessible
     base64ToPublicKey = crypto.getClass().getDeclaredMethod("base64ToPublicKey", String.class);
     base64ToPublicKey.setAccessible(true);
-    encryptAnswer = crypto.getClass().getDeclaredMethod("encryptAnswer", Output.class, PublicKey.class);
+    encryptAnswer = crypto.getClass().getDeclaredMethod("encryptAnswer", Output.class, SecretKey.class, IvParameterSpec.class);
     encryptAnswer.setAccessible(true);
   }
 
@@ -66,42 +101,100 @@ public class CryptoTest extends AndroidTestCase {
   }
 
   @Test
-  public void testEncryptAnswer() throws NoSuchAlgorithmException, InvocationTargetException, IllegalAccessException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+  public void testEncryptAnswer() throws NoSuchAlgorithmException, InvocationTargetException, IllegalAccessException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
     Output encryptedOutput = null;
     try {
-      encryptedOutput = (Output) encryptAnswer.invoke(crypto, output, keyPair.getPublic());
+      encryptedOutput = (Output) encryptAnswer.invoke(crypto, output, secretKey, ivParameterSpec);
     } catch (Exception e) {
       fail("Exception when trying to encrypt: " + e);
     }
 
-    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-    cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
-    String encryptedAnswer = encryptedOutput.getAnswer();
-    byte[] encryptedBytes = Base64.decode(encryptedAnswer, Base64.NO_WRAP);
-    byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+    byte[] decryptedBytes = decryptSymmetric(encryptedOutput.getAnswer(), secretKey, ivParameterSpec);
     String decryptedAnswer = new String(decryptedBytes, "UTF-8");
 
     assertEquals(decryptedAnswer, "answer");
   }
 
+  @Test
+  public void testCompleteEncryption() throws NoSuchPaddingException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, UnsupportedEncodingException, InvalidKeyException, InvalidKeySpecException {
+    Event event = new Event();
+    event.setResponses(outputList);
+    event.setServerExperimentId(42L);
+
+    Event encryptedEvent = crypto.encryptAnswers(event);
+    String encryptionKeyEncrypted = null;
+    String encryptionIvEncrypted = null;
+    String answerEncrypted = null;
+    for (Output response: encryptedEvent.getResponses()) {
+      if (response.getName().equals("encryptionKey"))
+        encryptionKeyEncrypted = response.getAnswer();
+      else if (response.getName().equals("encryptionIv"))
+        encryptionIvEncrypted = response.getAnswer();
+      else if (response.getName().equals("name"))
+        answerEncrypted = response.getAnswer();
+    }
+
+    byte[] symmetricKeyBytes = decryptAsymmetric(encryptionKeyEncrypted, keyPair.getPrivate());
+    byte[] ivBytes = decryptAsymmetric(encryptionIvEncrypted, keyPair.getPrivate());
+    SecretKeySpec privKey = new SecretKeySpec(symmetricKeyBytes, "AES");
+    IvParameterSpec ivspec = new IvParameterSpec(ivBytes);
+
+    String decryptedAnswer = new String(decryptSymmetric(answerEncrypted, privKey, ivspec), "UTF-8");
+
+    assertEquals(decryptedAnswer, "answer");
+  }
+
   /**
-   * This test makes sure that encrypting the same string twice would yield a different result. This
-   * is part of non-determinism, which is needed because otherwise someone with access to the server
-   * DB could encrypt a string of plaintext with the public key of the experiment organizer, and
-   * check if the encrypted text corresponds to any data in the database.
+   * This test makes sure that encrypting the same string twice would yield a different result for
+   * different Events. This is important, as we don't want information to leak about two answers
+   * that were the same for different events.
    */
   @Test
   public void testNonDeterminism() {
     try {
-      Output encryptedOutput1 = (Output) encryptAnswer.invoke(crypto, output, keyPair.getPublic());
-      Output output2 = new Output();
-      output2.setName("name");
-      output2.setAnswer("answer");
-      Output encryptedOutput2 = (Output) encryptAnswer.invoke(crypto, output2, keyPair.getPublic());
+      Event event1 = new Event();
+      event1.setResponses(outputList);
+      event1.setServerExperimentId(42L);
 
-      assertThat(encryptedOutput1.getAnswer(), not(equalTo(encryptedOutput2.getAnswer())));
+      Event event2 = new Event();
+      Output output2 = new Output();
+      output2.setName(output.getName());
+      output2.setAnswer(output.getAnswer());
+      event2.setResponses(Arrays.asList(output2));
+      event2.setServerExperimentId(42L);
+
+      Event encryptedEvent1 = crypto.encryptAnswers(event1);
+      Event encryptedEvent2 = crypto.encryptAnswers(event2);
+
+      String answer1 = null;
+      for (Output output : encryptedEvent1.getResponses()) {
+        if (output.getName().equals("name"))
+          answer1 = output.getAnswer();
+      }
+      String answer2 = null;
+      for (Output output : encryptedEvent2.getResponses()) {
+        if (output.getName().equals("name"))
+          answer2 = output.getAnswer();
+      }
+
+      assertThat(answer1, not(equalTo(answer2)));
     } catch (Exception e) {
       fail("Exception when trying to encrypt: " + e);
     }
+  }
+
+  private byte[] decryptAsymmetric(String encrypted, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+    Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+    cipher.init(Cipher.DECRYPT_MODE, privateKey);
+    byte[] encryptedBytes = Base64.decode(encrypted, Base64.NO_WRAP);
+    byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+    return decryptedBytes;
+  }
+
+  private byte[] decryptSymmetric(String encrypted, SecretKey secretKey, IvParameterSpec ivSpec) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
+    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+    cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
+    byte[] encryptedBytes = Base64.decode(encrypted, Base64.NO_WRAP);
+    return cipher.doFinal(encryptedBytes);
   }
 }
