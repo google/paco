314afe1e108af9fa8dc54fdb1d06c73fd00af4fe
indhumeyyappan <imeyyappan@google.com>
2017-05-18 15:03:40 -0700

    Missed to commit files, log messages

--- Paco-Server/src/com/google/sampling/experiential/server/migration/jobs/ExperimentTitleLowercaseMigrationJob.java
+++ Paco-Server/src/com/google/sampling/experiential/server/migration/jobs/ExperimentTitleLowercaseMigrationJob.java
@@ -74,7 +74,7 @@ public class ExperimentTitleLowercaseMigrationJob implements MigrationJob {
   }
 
   @Override
-  public boolean doMigration() {
+  public boolean doMigration(String cursor) {
     return doMigrationPublicExperiments();
   }
 
