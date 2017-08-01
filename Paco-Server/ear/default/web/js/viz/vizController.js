80c2440581566f92ff695603d07f6b1215e8da67
muthuk <muthuk@dhcp-172-19-56-45.mtv.corp.google.com>
2017-08-01 14:40:27 -0700

    Fix spacing issues
    Change 4 spaces to 2 spaces in viz.html, vizController.js,
    vizDirective.js, vizService.js and vizStyles.css.

--- Paco-Server/ear/default/web/js/viz/vizController.js
+++ Paco-Server/ear/default/web/js/viz/vizController.js
@@ -1,7 +1,6 @@
-"use strict";
+  "use strict";
 
 pacoApp.controller('VizCtrl', ['$scope', 'experimentsVizService', '$timeout', '$routeParams', function ($scope, experimentsVizService, $timeout, $routeParams) {
-
 
     $scope.responseTypeDetails = [];
     $scope.vizResponseData = [];
@@ -11,52 +10,52 @@ pacoApp.controller('VizCtrl', ['$scope', 'experimentsVizService', '$timeout', '$
     //experiment json objects are retrieved from the 'experimentsVizService'
     // to create a scope variable for response type meta data.
     $scope.getExperimentDetails = function () {
-        experimentsVizService.getExperiment($scope.experimentId).then(
-            function (experiment) {
-                console.log(experiment);
-                if (experiment.status === 404) {
-                    displayErrorMessage("Experiments ", experiment);
-                }
-                $scope.experimentDataModel = {
-                    id: experiment.results[0].id,
-                    title: experiment.results[0].title,
-                    creator: experiment.results[0].creator,
-                    date: experiment.results[0].modifyDate
-                };
-                experimentsVizService.getInputs(experiment.results[0]).forEach(function (inputType) {
-                    responseTypeList.push(inputType);
-                });
-            });
+      experimentsVizService.getExperiment($scope.experimentId).then(
+          function (experiment) {
+            console.log(experiment);
+            if (experiment.status === 404) {
+              displayErrorMessage("Experiments ", experiment);
+            }
+            $scope.experimentDataModel = {
+              id: experiment.results[0].id,
+              title: experiment.results[0].title,
+              creator: experiment.results[0].creator,
+              date: experiment.results[0].modifyDate
+            };
+            experimentsVizService.getInputs(experiment.results[0]).forEach(function (inputType) {
+              responseTypeList.push(inputType);
+          });
+      });
     };
     $scope.responseTypeDetails = responseTypeList;
 
     //events json objects are retrieved from the 'experimentsVizService'
     // to create a scope variable for participants' responses.
     $scope.getExperimentEvents = function () {
-        experimentsVizService.getEvents($scope.experimentId).then(
-            function (events) {
-                if (events.status === 404) {
-                    displayErrorMessage("Events ", events);
-                }
-                events.forEach(function (response) {
-                    experimentResponses.push(response);
-                })
+      experimentsVizService.getEvents($scope.experimentId).then(
+          function (events) {
+            if (events.status === 404) {
+              displayErrorMessage("Events ", events);
             }
-        )
+            events.forEach(function (response) {
+              experimentResponses.push(response);
+            })
+          }
+      )
     };
     $scope.vizResponseData = experimentResponses;
 
     if (angular.isDefined($routeParams.experimentId)) {
-        $scope.experimentId = parseInt($routeParams.experimentId, 10);
-        $scope.getExperimentDetails();
-        $scope.getExperimentEvents();
+      $scope.experimentId = parseInt($routeParams.experimentId, 10);
+      $scope.getExperimentDetails();
+      $scope.getExperimentEvents();
     }
 
     function displayErrorMessage(data, error) {
-        $scope.error = {
-            data: data,
-            code: error.status,
-            message: error.statusText
-        };
+      $scope.error = {
+        data: data,
+        code: error.status,
+        message: error.statusText
+      };
     }
 }]);
--- Paco-Server/ear/default/web/js/viz/vizService.js
+++ Paco-Server/ear/default/web/js/viz/vizService.js
@@ -1,114 +1,99 @@
 pacoApp.factory('experimentsVizService', ['$http', 'experimentService', function ($http, experimentService) {
 
-    var experiment = '';
+  var experiment = '';
 
-    function getExperiment(id) {
+  function getExperiment(id) {
 
-        var getExperiment = experimentService.getExperiment(id).then(function successCallback(experimentData) {
-            return experimentData.data;
-        }, function errorCallback(error) {
-            return error;
+    var getExperiment = experimentService.getExperiment(id).then(function successCallback(experimentData) {
+      return experimentData.data;
+    }, function errorCallback(error) {
+      return error;
+    });
+
+    experiment = getExperiment.then(function (experiment) {
+      return experiment;
+    });
+    return experiment;
+  }
+
+  function getInputs(experiment) {
+    var responseDetails = [];
+    var inputs = experiment.groups[0].inputs;
+    inputs.forEach(function (input) {
+      if (input.responseType == "likert") {
+        responseDetails.push({
+          "name": input.name,
+          "responseType": input.responseType,
+          "text": input.text,
+          "leftsidelabel": input.leftSideLabel,
+          "rightsidelabel": input.rightSideLabel
         });
-
-        experiment = getExperiment.then(function (experiment) {
-            return experiment;
+      } else if (input.responseType == "list") {
+        responseDetails.push({
+          "name": input.name,
+          "responseType": input.responseType,
+          "text": input.text,
+          "listChoices": input.listChoices
         });
-        return experiment;
-    }
+      } else {
+        responseDetails.push({"name": input.name, "responseType": input.responseType, "text": input.text});
+      }
+    });
+    return responseDetails;
+  }
 
-    function getInputs(experiment) {
-        var responseDetails = [];
-        var inputs = experiment.groups[0].inputs;
-        inputs.forEach(function (input) {
-            if (input.responseType == "likert") {
-                responseDetails.push({
-                    "name": input.name,
-                    "responseType": input.responseType,
-                    "text": input.text,
-                    "leftsidelabel": input.leftSideLabel,
-                    "rightsidelabel": input.rightSideLabel
-                });
-            } else if (input.responseType == "list") {
-                responseDetails.push({
-                    "name": input.name,
-                    "responseType": input.responseType,
-                    "text": input.text,
-                    "listChoices": input.listChoices
-                });
-            } else {
-                responseDetails.push({"name": input.name, "responseType": input.responseType, "text": input.text});
+  function getEvents(id) {
+
+    var getEvents = $http({
+      method: 'GET',
+      url: '/events?q=experimentId=' + id + '&json'
+    }).then(function successCallback(response) {
+      return response.data.events;
+    }, function errorCallback(error) {
+      return error;
+    });
+
+    var answers = {};
+    var responseResults = "";
+    var responses = [];
+
+    var getResponses = getEvents.then(function (events) {
+      if (events.status === 404) {
+        return events;
+      } else {
+        events.forEach(function (event) {
+          answers.who = event.who;
+          answers.when = event.when;
+          answers.responseTime = event.responseTime;
+          events.push(event.responses);
+          event.responses.forEach(function (e) {
+            answers.name = e.name;
+            answers.answer = e.answer;
+            if (answers.name != "joined") {
+              responses.push({
+                "who": answers.who,
+                "when": answers.when,
+                "responseTime": answers.responseTime,
+                "name": answers.name,
+                "answer": answers.answer
+              });
             }
+          });
         });
-        return responseDetails;
-    }
-
-    function getEvents(id) {
-
-        var getEvents = $http({
-            method: 'GET',
-            url: '/events?q=experimentId=' + id + '&json'
-        }).then(function successCallback(response) {
-            return response.data.events;
-        }, function errorCallback(error) {
-            return error;
-        });
-
-        //TODO
-        // var message = '{ query : { criteria : "experiment_id = ? and group_name = ?", values : [5770237022568448, "New Group"]}, order : response_time}';
-
-        // var message = '{ query : { criteria : "experiment_id = ? and group_name = ?", values : [' + id + ', "' + group_name + '"]}}';
-        // var fetchEvents = $http({
-        //     method: 'POST',
-        //     url: '/csSearch',
-        //     data: {'message': message}
-        // }).then(function successCallback(response) {
-        //     console.log(response);
-        //     return response;
-        // }, function errorCallback(error) {
-        //     console.log(error);
-        // });
-
-        var answers = {};
-        var responseResults = "";
-        var responses = [];
-
-        var getResponses = getEvents.then(function (events) {
-            if (events.status === 404) {
-                return events;
-            } else {
-                events.forEach(function (event) {
-                    answers.who = event.who;
-                    answers.when = event.when;
-                    answers.responseTime = event.responseTime;
-                    events.push(event.responses);
-                    event.responses.forEach(function (e) {
-                        answers.name = e.name;
-                        answers.answer = e.answer;
-                        if (answers.name != "joined") {
-                            responses.push({
-                                "who": answers.who,
-                                "when": answers.when,
-                                "responseTime": answers.responseTime,
-                                "name": answers.name,
-                                "answer": answers.answer
-                            });
-                        }
-                    });
-                });
-                var groupByInputs = d3.nest()
-                    .key(function (d) {
-                        return d.name;
-                    })
-                    .entries(responses);
-                return groupByInputs;
-            }
-        });
-        responseResults = getResponses;
-        return responseResults;
-    }
-    return {
-        getEvents: getEvents,
-        getInputs: getInputs,
-        getExperiment: getExperiment
-    }
+        var groupByInputs = d3.nest()
+            .key(function (d) {
+              return d.name;
+            })
+            .entries(responses);
+        return groupByInputs;
+      }
+    });
+    responseResults = getResponses;
+    return responseResults;
+  }
+  return {
+    getEvents: getEvents,
+    getInputs: getInputs,
+    getExperiment: getExperiment
+  }
 }]);
