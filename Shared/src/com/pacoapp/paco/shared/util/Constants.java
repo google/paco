9b9ba3ae67b9463ed958d6094f491e6eab676293
imeyyappan <imeyyappan@dhcp-100-99-164-10.mtv.corp.google.com>
2018-08-23 11:46:32 -0700

    Enable hack interface

--- Shared/src/com/pacoapp/paco/shared/util/Constants.java
+++ Shared/src/com/pacoapp/paco/shared/util/Constants.java
@@ -45,7 +45,7 @@ public class Constants {
   public static final String RESPONSE_TYPE_APP_JSON = "application/json";
   public static final String UNKNOWN = "unknown";
   public static final Boolean USE_OLD_FORMAT_FLAG = false;
-  public static final Boolean USE_OLD_SEARCH_QUERY = true;
+  public static final Boolean USE_OLD_SEARCH_QUERY = false;
   public static final Boolean PERSIST_IN_CLOUD_SQL_FLAG = false;
   public static final Boolean SEND_TO_CLOUD_SQL = true;
   public static final String IOS = "iOS";
